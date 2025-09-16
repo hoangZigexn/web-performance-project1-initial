9000740905bdbd177879ed334f23631de0a8be9d
hoangpv-zigexn <hoangpv@zigexn.vn>
2025-09-16 15:41:03 +0700

    test

---
+++ scripts/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "=== TESTING PROJECT ==="
+
+
+echo "Running tests..."
+echo $PWD
+# npm run test:ci
+
+# # Check if installation was successful
+# if [ $? -eq 0 ]; then
+#     echo "✅ Tests passed successfully"
+# else
+#     echo "❌ Failed to run tests"
+#     exit 1
+# fi
